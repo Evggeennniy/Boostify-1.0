cf23fc2c7e1a6b1f25837b53eea7a8b96333efa8
FiX <myiphone128@mail.ru>
2024-08-08 02:09:34 +0300

    fixes

--- react_app/src/Config.js
+++ react_app/src/Config.js
@@ -1,9 +1,13 @@
 export let staticPath;
-export const apiUrlServices = "http://127.0.0.1:53433/api/services";
-export const apiUrlCreate = "http://127.0.0.1:53433/api/create_bill";
+export let apiUrlServices;
+export let apiUrlCreate;
 
 if (process.env.NODE_ENV === "production") {
   staticPath = "/static";
+  apiUrlServices = "/api/services";
+  apiUrlCreate = "/api/create_bill";
 } else {
   staticPath = "http://127.0.0.1:53433/static";
+  apiUrlServices = "http://127.0.0.1:53433/api/services";
+  apiUrlCreate = "http://127.0.0.1:53433/api/create_bill";
 }
