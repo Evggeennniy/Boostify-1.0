8288b7c268585f882faef3f5ac3ed8dd5fc7cf63
Evgeniy <myiphone128@mail.ru>
2024-08-08 16:36:19 +0300

    fixes

--- react_app/src/Config.js
+++ react_app/src/Config.js
@@ -1,6 +1,6 @@
 export let staticPath = "/static";
-export let apiUrlServices = "http://142.93.104.28:80/api/services";
-export let apiUrlCreate = "http://142.93.104.28:80/api/create_bill";
+export let apiUrlServices = "https://boostify.space/api/services";
+export let apiUrlCreate = "https://boostify.space/api/create_bill";
 
 // export let  staticPath = "http://127.0.0.1:53433/static";
 // export let  apiUrlServices = "http://127.0.0.1:53433/api/services";
