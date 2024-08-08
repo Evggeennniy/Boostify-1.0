d70ea86c42f6db5934b8ed0d67c307cadc5d924a
FiX <myiphone128@mail.ru>
2024-08-08 17:34:24 +0300

    fixes

--- telegram_app/app.py
+++ telegram_app/app.py
@@ -11,7 +11,6 @@ HOST = '127.0.0.1'
 PORT = 11211
 telegram_api_key = os.getenv('TG_API')
 application = Application.builder().token(telegram_api_key).build()
-allowed_user = 1306750810
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -22,7 +21,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         if cached_orders:
             for order in cached_orders:
                 await context.bot.send_message(chat_id=1306750810, text=order)
-            print('Заказы отправлены администратору')
+            for order in cached_orders:
+                await context.bot.send_message(chat_id=414902937, text=order)
+            print('Заказы отправлены администраторам')
 
             await acache.set('cached_orders', [])
         else:
@@ -33,7 +34,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 def main() -> None:
-    application.add_handler(CommandHandler('startChecking128', start))
+    application.add_handler(CommandHandler('startChecking128821', start))
     # application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     application.run_polling()
 
