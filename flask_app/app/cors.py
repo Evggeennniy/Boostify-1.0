6932e9f3b489436ebdaac69cad0a156477d98291
FiX <myiphone128@mail.ru>
2025-03-16 06:52:12 +0200

    cors fix

--- flask_app/app/cors.py
+++ flask_app/app/cors.py
@@ -2,4 +2,5 @@ from flask_cors import CORS
 
 
 def init_cors(app):
-    CORS(app, resources={r"/api/*": {"origins": "*"}})
+    CORS(app, resources={
+         r"/api/*": {"origins": ["http://localhost:3000", "https://capilike.mom"]}})
